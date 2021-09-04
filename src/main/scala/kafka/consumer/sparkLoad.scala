def08fad03dd49fab74688a7e94ee86b03ecc65d
Subhojit89868 <subhojit.jamshedpur@gmail.com>
2021-09-04 23:01:39 +0530

    Updated logs

--- src/main/scala/kafka/consumer/sparkLoad.scala
+++ src/main/scala/kafka/consumer/sparkLoad.scala
@@ -3,7 +3,6 @@ package kafka.consumer
 import com.typesafe.config.Config
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.functions.{current_timestamp, date_format}
 import resource.ingestSchema
 
 object sparkLoad {
@@ -16,31 +15,23 @@ object sparkLoad {
       .getOrCreate()
     val data = rdd
       .map(_.split(",").to[List])
-      .map(line => row(line))
+      .map(line => transform(line))
 
     val userSchema = ingestSchema(config) //Method from resource.ingestSchema
     val rawDF = spark.createDataFrame(data, userSchema)
 
-    val fin = rawDF
-      .withColumn("etl_ts", current_timestamp)
-      .withColumn("year", date_format(current_timestamp, "yyyy"))
-      .withColumn("month", date_format(current_timestamp, "MM"))
-      .withColumn("day", date_format(current_timestamp, "dd"))
-
-    fin.coalesce(1)
-      .write
-      .partitionBy("year","month","day")
+    rawDF.write
       .mode("append")
-      .parquet("data/output/")
+      .parquet("data/output/tmp/")
   }
 
-  def row(column:List[String]): Row ={
-    if(column.nonEmpty){
-      var columnAppend = Seq(column.head)
-      for(i <- 1 until column.length){
-        columnAppend = columnAppend ++ Seq(column(i))
+  def transform(line:List[String]): Row ={
+    if(line.nonEmpty){
+      var lineAppend = Seq(line.head)
+      for(i <- 1 until line.length){
+        lineAppend = lineAppend ++ Seq(line(i))
       }
-      Row.fromSeq(columnAppend)
+      Row.fromSeq(lineAppend)
     }
     else
       Row()
--- src/main/scala/mainIngestion.scala
+++ src/main/scala/mainIngestion.scala
@@ -1,24 +1,31 @@
 import java.io.File
+import java.time.LocalDateTime
 import com.typesafe.config.ConfigFactory
 import kafka.consumer.kafkaConsumer.consumer
 import kafka.consumer.sparkLoad.ingest
+import org.apache.commons.io.FileUtils
 import org.apache.log4j._
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{current_timestamp, date_format}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object mainIngestion{
+
   def main(args: Array[String]): Unit ={
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
+    println(LocalDateTime.now() + " : Sourcing Config file.")
     val configPath = System.getProperty("user.dir") + "\\src\\main\\conf\\"
     val config = ConfigFactory.parseFile(new File(configPath + "consumer.conf"))
+    println(LocalDateTime.now() + " : Sourced Config Object : " + config)
 
     val sparkconf = new SparkConf()
       .setMaster("local")
       .setAppName("kafkaConsumer")
-
     val ssc = new StreamingContext(sparkconf, Seconds(config.getString("streamPeriod").toInt))
+
     val message = consumer(ssc, config) // Method from kafka.consumer.kafkaConsumer.consumer
 
     val lines = message
@@ -26,11 +33,38 @@ object mainIngestion{
       .flatMap(_.split("\n"))
 
     lines.foreachRDD {
-      rdd => ingest(rdd, config) //Method from kafka.consumer.sparkLoad.ingest
+      rdd => println(LocalDateTime.now() + " : Storing Raw messages in tmp directory.")
+        ingest(rdd, config) //Method from kafka.consumer.sparkLoad.ingest
     }
 
+    println(LocalDateTime.now() + " : Starting Streaming Context.")
     ssc.start()
     ssc.awaitTerminationOrTimeout(config.getString("streamTimeout").toInt)
+    println(LocalDateTime.now() + " : Streaming Context timed out, hence stopping.")
     ssc.stop()
+
+    val spark = SparkSession
+      .builder
+      .master("local")
+      .getOrCreate()
+    println(LocalDateTime.now() + " : Writing final data into target directory")
+    write(spark)
+  }
+
+  def write(spark: SparkSession): Unit ={
+    spark.read
+      .parquet("data/output/tmp/*.parquet")
+      .withColumn("etl_ts", current_timestamp)
+      .withColumn("year", date_format(current_timestamp, "yyyy"))
+      .withColumn("month", date_format(current_timestamp, "MM"))
+      .withColumn("day", date_format(current_timestamp, "dd"))
+      .orderBy("id")
+      .repartition(1)
+      .write
+      .mode("append")
+      .partitionBy("year","month","day")
+      .parquet("data/output/partition/")
+    println(LocalDateTime.now() + " : Deleting preprocessed tmp file.")
+    FileUtils.deleteQuietly(new File("data/output/tmp/"))
   }
 }
